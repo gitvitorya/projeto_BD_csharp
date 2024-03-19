1ca77f5dd73c89a7aed9e670f6d091fc93b9d232
gitvitorya <124717307+gitvitorya@users.noreply.github.com>
2024-03-19 09:33:32 -0300

    subindo projeto

---
+++ ProjetoBD/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+using System;
+
+namespace ProjetoBD {
+    partial class Form1 {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(238, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(210, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "BUSCAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Insira um ID:";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(40, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(159, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(40, 145);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(159, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Insira um produto:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(40, 224);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(159, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Insira um preço:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(40, 279);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 43);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "INSERIR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(471, 52);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(179, 37);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "DELETAR";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // Não há implementação necessária aqui
+        }
+
+
+        private void label2_Click(object sender, EventArgs e) {
+            throw new NotImplementedException();
+        }
+
+        private void label1_Click_1(object sender, EventArgs e) {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ ProjetoBD/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoBD {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        SQLiteConnection conexao;
+
+        public SQLiteConnection ObterConexao() {
+            conexao = new SQLiteConnection("Data Source=C:\\Users\\vitorya.candido\\source\\repos\\ProjetoBD\\database.db");
+            conexao.Open();
+            return conexao;
+        }
+
+        public DataTable ObterProdutos() {
+            SQLiteDataAdapter da;
+            DataTable dt = new DataTable();
+
+            var cmd = ObterConexao().CreateCommand();
+            cmd.CommandText = "SELECT * FROM produtos WHERE id='" + textBox1.Text + "'"; // pega o id digitado pelo user
+
+
+            da = new SQLiteDataAdapter(cmd.CommandText, ObterConexao());
+
+
+            da.Fill(dt);
+
+            return dt;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e) {
+            DataTable dt = ObterProdutos();
+            if (dt.Rows.Count > 0) {
+                string dado = dt.Rows[0].ItemArray[1].ToString(); //mostra o nome 
+                MessageBox.Show(dado);
+            }
+            else {
+                MessageBox.Show("Nenhum produto encontrado com o ID especificado.");
+            }
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e) {
+            var cmd = ObterConexao().CreateCommand();
+            cmd.CommandText = "INSERT INTO produtos(id, nome, preco) VALUES (@id,@nome,@preco)";
+            cmd.Parameters.AddWithValue("@id", textBox1.Text);
+            cmd.Parameters.AddWithValue("@nome", textBox2.Text);
+            cmd.Parameters.AddWithValue("@preco", textBox3.Text);
+
+            cmd.ExecuteNonQuery();
+            ObterConexao().Close();
+            MessageBox.Show("NOVO PRODUTO ADICIONADO");
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            var cmd = ObterConexao().CreateCommand();
+            cmd.CommandText = "DELETE FROM produtos WHERE id='" + textBox1.Text + "'";
+            
+
+            cmd.ExecuteNonQuery();
+            ObterConexao().Close();
+            MessageBox.Show("PRODUTO DELETADO");
+        }
+    }
+}
